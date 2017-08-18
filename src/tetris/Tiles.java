65b1034e683a76d2451cb833e695535b43479f09
Adrian Alano <adrian_alano@dlsu.edu.ph>
2017-08-18 22:36:04 +0800

    Update and rename TileType.java to Tiles.java

--- src/tetris/Tiles.java
+++ src/tetris/Tiles.java
@@ -7,8 +7,8 @@ package tetris;
 
 import java.awt.Color;
 
-public enum TileType {
-	TypeI(new Color(BoardPanel.COLOR_MIN, BoardPanel.COLOR_MAX, BoardPanel.COLOR_MAX), 4, 4, 1, new boolean[][] {
+public enum Tiles {
+	TypeI(new Color(Main.COLOR_MIN, Main.COLOR_MAX, Main.COLOR_MAX), 4, 4, 1, new boolean[][] {
 		{
 			false,	false,	false,	false,
 			true,	true,	true,	true,
@@ -34,7 +34,7 @@ public enum TileType {
 			false,	true,	false,	false,
 		}
 	}),
-	TypeJ(new Color(BoardPanel.COLOR_MIN, BoardPanel.COLOR_MIN, BoardPanel.COLOR_MAX), 3, 3, 2, new boolean[][] {
+	TypeJ(new Color(Main.COLOR_MIN, Main.COLOR_MIN, Main.COLOR_MAX), 3, 3, 2, new boolean[][] {
 		{
 			true,	false,	false,
 			true,	true,	true,
@@ -56,7 +56,7 @@ public enum TileType {
 			true,	true,	false,
 		}
 	}),
-	TypeL(new Color(BoardPanel.COLOR_MAX, 127, BoardPanel.COLOR_MIN), 3, 3, 2, new boolean[][] {
+	TypeL(new Color(Main.COLOR_MAX, 127, Main.COLOR_MIN), 3, 3, 2, new boolean[][] {
 		{
 			false,	false,	true,
 			true,	true,	true,
@@ -78,7 +78,7 @@ public enum TileType {
 			false,	true,	false,
 		}
 	}),
-	TypeO(new Color(BoardPanel.COLOR_MAX, BoardPanel.COLOR_MAX, BoardPanel.COLOR_MIN), 2, 2, 2, new boolean[][] {
+	TypeO(new Color(Main.COLOR_MAX, Main.COLOR_MAX, Main.COLOR_MIN), 2, 2, 2, new boolean[][] {
 		{
 			true,	true,
 			true,	true,
@@ -96,7 +96,7 @@ public enum TileType {
 			true,	true,
 		}
 	}),
-	TypeS(new Color(BoardPanel.COLOR_MIN, BoardPanel.COLOR_MAX, BoardPanel.COLOR_MIN), 3, 3, 2, new boolean[][] {
+	TypeS(new Color(Main.COLOR_MIN, Main.COLOR_MAX, Main.COLOR_MIN), 3, 3, 2, new boolean[][] {
 		{
 			false,	true,	true,
 			true,	true,	false,
@@ -118,7 +118,7 @@ public enum TileType {
 			false,	true,	false,
 		}
 	}),
-	TypeT(new Color(128, BoardPanel.COLOR_MIN, 128), 3, 3, 2, new boolean[][] {
+	TypeT(new Color(128, Main.COLOR_MIN, 128), 3, 3, 2, new boolean[][] {
 		{
 			false,	true,	false,
 			true,	true,	true,
@@ -140,7 +140,7 @@ public enum TileType {
 			false,	true,	false,
 		}
 	}),
-	TypeZ(new Color(BoardPanel.COLOR_MAX, BoardPanel.COLOR_MIN, BoardPanel.COLOR_MIN), 3, 3, 2, new boolean[][] {
+	TypeZ(new Color(Main.COLOR_MAX, Main.COLOR_MIN, Main.COLOR_MIN), 3, 3, 2, new boolean[][] {
 		{
 			true,	true,	false,
 			false,	true,	true,
@@ -162,16 +162,16 @@ public enum TileType {
 			true,	false,	false,
 		}
 	});
-	private Color baseColor;
-	private Color lightColor;
-	private Color darkColor;
-	private int spawnCol;
-	private int spawnRow;
-	private int dimension;
-	private int rows;
-	private int cols;
-	private boolean[][] tiles;
-	private TileType(Color color, int dimension, int cols, int rows, boolean[][] tiles) {
+	private final Color baseColor;
+	private final Color lightColor;
+	private final Color darkColor;
+	private final int spawnCol;
+	private final int spawnRow;
+	private final int dimension;
+	private final int rows;
+	private final int cols;
+	private final boolean[][] tiles;
+	private Tiles(Color color, int dimension, int cols, int rows, boolean[][] tiles) {
 		this.baseColor = color;
 		this.lightColor = color.brighter();
 		this.darkColor = color.darker();
@@ -244,6 +244,49 @@ public enum TileType {
 		for(int y = dimension - 1; y >= 0; y--) {
 			for(int x = 0; x < dimension; x++) {
 				if(isTile(x, y, rotation)) {
+					return dimension - y;
+				}
+			}
+		}
+		return -1;
+	}
+        public boolean aTile(int x, int y, int rotation) {
+		return tiles[rotation][y * dimension + x];
+	}
+	public int getLInset(int rotation) {
+		for(int x = 0; x < dimension; x++) {
+			for(int y = 0; y < dimension; y++) {
+				if(aTile(x, y, rotation)) {
+					return x;
+				}
+			}
+		}
+		return -1;
+	}
+	public int getRInset(int rotation) {
+		for(int x = dimension - 1; x >= 0; x--) {
+			for(int y = 0; y < dimension; y++) {
+				if(aTile(x, y, rotation)) {
+					return dimension - x;
+				}
+			}
+		}
+		return -1;
+	}
+	public int getTInset(int rotation) {
+		for(int y = 0; y < dimension; y++) {
+			for(int x = 0; x < dimension; x++) {
+				if(aTile(x, y, rotation)) {
+					return y;
+				}
+			}
+		}
+		return -1;
+	}
+	public int getBInset(int rotation) {
+		for(int y = dimension - 1; y >= 0; y--) {
+			for(int x = 0; x < dimension; x++) {
+				if(aTile(x, y, rotation)) {
 					return dimension - y;
 				}
 			}
